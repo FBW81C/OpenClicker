8730073b15563e39cdace8388d56c05f0890afd3
FBW81C <fbw81c@gmail.com>
2025-08-21 20:52:48 +0200

    POC clicking logic but not for holding, currently unstable version with a lot of useless code

---
+++ OpenClicker/Exceptions/InvalidClickPatternException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClicker.Exceptions;
+public class InvalidClickPatternException : Exception
+{
+    public InvalidClickPatternException()
+    {
+    }
+
+    public InvalidClickPatternException(string? message) : base(message)
+    {
+    }
+
+    public InvalidClickPatternException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected InvalidClickPatternException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
--- OpenClicker/Lib/PickLocation.cs
+++ OpenClicker/Lib/PickLocation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenClicker;
+namespace OpenClicker.Lib;
 
 public static class PickLocation
 {
