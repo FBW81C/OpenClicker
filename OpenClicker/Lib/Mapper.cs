439a2aa166c9713e3d20b5c9014e5700ceb51ab0
Lukas Bühlmann BIT <lukas.buehlmann@bit.admin.ch>
2025-09-10 08:24:19 +0200

    Added Click Recorder

---
+++ OpenClicker/Lib/Mapper.cs
@@ -0,0 +1,16 @@
+﻿using OpenClicker.Models;
+
+namespace OpenClicker.Lib;
+public static class Mapper
+{
+    public static OCMouseButtons MapMouseButton(MouseButtons button)
+    {
+        return button switch
+        {
+            MouseButtons.Left => OCMouseButtons.Left,
+            MouseButtons.Right => OCMouseButtons.Right,
+            MouseButtons.Middle => OCMouseButtons.Middle,
+            _ => OCMouseButtons.Left,
+        };
+    }
+}
