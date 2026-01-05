b933317bc57821f7c512b4626ffa7400f1eb6855
FBW81C <fbw81c@gmail.com>
2026-01-05 13:55:32 +0100

    Switched from legacy WinAPI to new WinAPI

---
+++ OpenClicker/Lib/NativeInput.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace OpenClicker.Lib;
+
+public static class NativeInput
+{
+    public const uint INPUT_MOUSE = 0;
+    public const uint INPUT_KEYBOARD = 1;
+    public const uint INPUT_HARDWARE = 2;
+
+    public const uint KEYEVENTF_KEYUP = 0x0002;
+
+    public const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+    public const uint MOUSEEVENTF_LEFTUP = 0x0004;
+    public const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+    public const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+    public const uint MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+    public const uint MOUSEEVENTF_MIDDLEUP = 0x0040;
+
+    // Für absolute Bewegung
+    public const uint MOUSEEVENTF_MOVE = 0x0001;
+    public const uint MOUSEEVENTF_ABSOLUTE = 0x8000;
+    public const uint MOUSEEVENTF_VIRTUALDESK = 0x4000;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern uint SendInput(
+        uint nInputs,
+        INPUT[] pInputs,
+        int cbSize);
+
+    //[DllImport("user32.dll")]
+    //public static extern bool SetCursorPos(int x, int y);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct INPUT
+    {
+        public uint type;
+        public INPUTUNION u;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct INPUTUNION
+    {
+        [FieldOffset(0)] public MOUSEINPUT mi;
+        [FieldOffset(0)] public KEYBDINPUT ki;
+        [FieldOffset(0)] public HARDWAREINPUT hi;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MOUSEINPUT
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct KEYBDINPUT
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HARDWAREINPUT
+    {
+        public uint uMsg;
+        public ushort wParamL;
+        public ushort wParamH;
+    }
+}
--- OpenClicker/Program.cs
+++ OpenClicker/Program.cs
@@ -1,5 +1,6 @@
 using OpenClicker.Forms.Hotkeys;
 using OpenClicker.Forms.Main;
+using OpenClicker.Lib;
 using OpenClicker.Models;
 using OpenClicker.Models;
 using System.Runtime.InteropServices;
@@ -9,18 +10,6 @@ namespace OpenClicker;
 
 static class Program
 {
-    private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-    private const int MOUSEEVENTF_LEFTUP = 0x04;
-    private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-    private const int MOUSEEVENTF_RIGHTUP = 0x10;
-    private const int MOUSEEVENTF_MIDDLEDOWN = 0x20;
-    private const int MOUSEEVENTF_MIDDLEUP = 0x40;
-
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
-    [DllImport("user32.dll")]
-    private static extern bool SetCursorPos(int X, int Y);
-
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
@@ -47,87 +36,6 @@ static class Program
         string? fileToOpen = args.Length > 0 ? args[0] : null;
 
         Application.Run(new Main(hotkeyManager, fileToOpen));
-    }
-
-    private static void LeftClick()
-    {
-        PressMouseButton(MOUSEEVENTF_LEFTDOWN);
-        PressMouseButton(MOUSEEVENTF_LEFTUP);
-    }
-    private static void RightClick()
-    {
-        PressMouseButton(MOUSEEVENTF_RIGHTDOWN);
-        PressMouseButton(MOUSEEVENTF_RIGHTUP);
-    }
-    private static void MiddleClick()
-    {
-        PressMouseButton(MOUSEEVENTF_MIDDLEDOWN);
-        PressMouseButton(MOUSEEVENTF_MIDDLEUP);
-    }
-
-    private static void PressMouseButton(int button)
-    {
-        mouse_event(button, 0, 0, 0, IntPtr.Zero);
-    }
-
-    public static void Click(ClickTypes type, OCMouseButtons button, Point? point = null, bool? twice = false)
-    {
-        if (type == ClickTypes.Hold)
-        {
-            throw new InvalidOperationException();
-        }
-
-        if (point != null)
-        {
-            SetCursorPos(point.Value.X, point.Value.Y);
-        }
-
-        ClickWithButton(button);
-        if (type == ClickTypes.Double)
-        {
-            ClickWithButton(button);
-        }
-    }
-
-    private static void ClickWithButton(OCMouseButtons button)
-    {
-        switch (button)
-        {
-            case OCMouseButtons.Left:
-                LeftClick();
-                break;
-            case OCMouseButtons.Right:
-                RightClick();
-                break;
-            case OCMouseButtons.Middle:
-                MiddleClick();
-                break;
-            default:
-                break;
-        }
-    }
-
-    public static void ToggleMouseButton(OCMouseButtons button, bool down, Point? point = null)
-    {
-        if (point != null)
-        {
-            SetCursorPos(point.Value.X, point.Value.Y);
-        }
-
-        switch (button)
-        {
-            case OCMouseButtons.Left:
-                PressMouseButton(down ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_LEFTUP);
-                break;
-            case OCMouseButtons.Right:
-                PressMouseButton(down ? MOUSEEVENTF_RIGHTDOWN : MOUSEEVENTF_RIGHTUP);
-                break;
-            case OCMouseButtons.Middle:
-                PressMouseButton(down ? MOUSEEVENTF_MIDDLEDOWN : MOUSEEVENTF_MIDDLEUP);
-                break;
-            default:
-                break;
-        }
     }
 
     public static void CreateSettingsFolderIfNotExist()
